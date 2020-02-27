702573486f3cdf0036a9d7106793dd94be6a04c8
reireynoso <reireynoso@gmail.com>
2020-02-27 08:47:31 -0500

    css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,13 @@ const Header = () => {
     return (
         <div className="nav">
             <div className="page-links">
-                <Link to="/">Home</Link>
+                <Link className="links" to="/">Home</Link>
+                <Link className="links" to="/About">About</Link>
             </div>
 
             <div className="icons">
-                
+                <div>Somthing</div>
+                <div>Else</div>
             </div>
         </div>
     )

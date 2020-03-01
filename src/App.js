3599b2b88d49512db9d2e3f0334665605330d5e0
reireynoso <reireynoso@gmail.com>
2020-02-29 22:55:35 -0500

    design changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
-import Header from './components/Header'
 import Home from './components/Home'
 
 const App = () => {
   return (
     <div className="App">
-        {/* <Header /> */}
-        <Header/>
         <Home />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import Header from './Header'
 const Home = () => {
     return (
         <div className="landing-page">
-            
             <h1 className="lg-heading">Reinald <span className="last-name">Reynoso</span></h1>
             <h2 className="sm-heading">Full Stack Web Developer</h2>
+            <Header/>
         </div>
     )
 }

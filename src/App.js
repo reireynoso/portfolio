f55a51c6492428ede9087aac7058c7c98a5e7cb0
reireynoso <reireynoso@gmail.com>
2020-03-01 15:24:42 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
+import About from './components/About'
 
 const App = () => {
   return (
     <div className="App">
-        <Home />
+        <Switch>
+          <Route path="/about" component={About}/>
+          <Route exact path="/" component={Home}/> 
+        </Switch>
     </div>
   );
 }

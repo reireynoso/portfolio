38999b09e546bee38e7c7836a441918e2a08b0fe
reireynoso <reireynoso@gmail.com>
2020-03-02 11:25:53 -0500

    image css

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,13 @@ const About = () => {
     return (
         
             <div className="link-container">
-                <h1>About</h1>
-                <p>Flatiron School graduate with a focus in full stack web development, four years experience teaching middle school math, Bachelor’s Degree in Mathematics, and a Master’s Degree in Educational Technology. I bring strong skills in management, instruction and communication as well as an exceptional background with complex problem solving and educational technology implementation. I have experience with React, Redux, Javascript, Ruby, Ruby on Rails, HTML/CSS and Restful API.</p>
+                <h1 className="content-title">About</h1>
+                <div>
+                    <div className="image">
+                        <img className="profile" src="https://raw.githubusercontent.com/reireynoso/modern_portfolio/master/dist/img/personal.JPG"/>
+                    </div>
+                    <p className="content-info">Flatiron School graduate with a focus in full stack web development, four years experience teaching middle school math, Bachelor’s Degree in Mathematics, and a Master’s Degree in Educational Technology. I bring strong skills in management, instruction and communication as well as an exceptional background with complex problem solving and educational technology implementation. I have experience with React, Redux, Javascript, Ruby, Ruby on Rails, HTML/CSS and Restful API.</p>
+                </div>
             </div>
     )
 }

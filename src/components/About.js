40fd2bba48b26394ca9791c488ef08c1c53d3c9c
reireynoso <reireynoso@gmail.com>
2020-03-02 09:42:09 -0500

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        
+            <div className="link-container">
+                <h1>About</h1>
+                <p>Flatiron School graduate with a focus in full stack web development, four years experience teaching middle school math, Bachelor’s Degree in Mathematics, and a Master’s Degree in Educational Technology. I bring strong skills in management, instruction and communication as well as an exceptional background with complex problem solving and educational technology implementation. I have experience with React, Redux, Javascript, Ruby, Ruby on Rails, HTML/CSS and Restful API.</p>
+            </div>
     )
 }
 

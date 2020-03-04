d2a78f51112310ea9698708a25dd97ada3f821fc
reireynoso <reireynoso@gmail.com>
2020-03-04 12:30:39 -0500

    project css and components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
 import About from './components/About'
+import Project from './components/Project'
 
 const App = () => {
   return (
     <div className="App">
         <Switch>
+          <Route path="/projects" component={Project}/>
           <Route path="/about" component={About}/>
           <Route exact path="/" component={Home}/> 
         </Switch>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import HomeIcon from './HomeIcon'
 const Project = () => {
+
     return (
-        <div>
-            Project
+        <div className="link-container">
+            <HomeIcon/>
+            <h1 className="project-content-title">Projects</h1>
+            <div className="project-container">
+                <h2 className="project-title">Project Title</h2>
+                <img className="project-image" src="https://i.ytimg.com/vi/WxmL6egpkKs/hqdefault.jpg" alt=""/>
+                <p className="project-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                <div className="project-buttons">
+                    <button>Demo</button>
+                    <button>Repo</button>
+                    <button>Site</button>
+                </div>
+            </div>
         </div>
     )
 }

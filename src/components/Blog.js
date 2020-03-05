5e320440cb63695d5c3cc94fd2edcf779873cfa6
reireynoso <reireynoso@gmail.com>
2020-03-05 15:32:47 -0500

    blog component change

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomeIcon from './HomeIcon';
+import { FaMedium } from "react-icons/fa"
 
 const Blog = () => {
     return(
@@ -7,7 +8,10 @@ const Blog = () => {
             <HomeIcon/>
             <h1 className="blog-content-title">blogs</h1>
             <div className="blog-container">
-                <p className="blog-handle">Medium</p>
+                <p className="blog-handle">
+                    <div><FaMedium/>Medium</div>
+                    <span className="handle">@reireynoso</span>
+                </p>
                 <img className="blog-image" src="https://miro.medium.com/fit/c/2100/630/0*1JeHUV1wSFYeWTcU" alt=""/>
                 <h2 className="blog-title">blog Title</h2>
                 <p className="blog-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.

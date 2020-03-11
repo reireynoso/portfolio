7e742db5ca9327801e5f6b0637759f74bd9b4225
reireynoso <reireynoso@gmail.com>
2020-03-11 14:39:22 -0400

    resume component and css update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,17 +9,17 @@ const Resume = () => {
             <div className="education-container">
                 <h2 className="resume-content-subtitle">Education</h2>
                 <div className="education-card">
-                    <h2>Rutgers - Newark</h2>
-                    <p>B.A in Mathematics</p>
+                    <h2>RUTGERS - NEWARK</h2>
+                    <p>Newark, NJ - 2013</p>
+                    <p>B.A in Mathematics / Minor in Computer Science</p>
                 </div>
             </div>
 
             <div className="work-container">
                 <h2 className="resume-content-subtitle">Employment History</h2>
-                <div className>
+                <div className="work-card">
                     <h2>Software Engineering Coach</h2>
-                    <p>Company: WeWork x Flatiron School</p>
-                    <p>Duties</p>
+                    <p>WeWork x Flatiron School | Aug 2019 - Current</p>
                     <ul>
                         <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.

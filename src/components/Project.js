642ef1a8ae5a8956951372a22c3b59c187b356e7
reireynoso <reireynoso@gmail.com>
2020-03-12 15:29:04 -0400

    created project card component and dymanically rendered projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomeIcon from './HomeIcon'
+import ProjectCard from './ProjectCard'
 import information from '../information'
 
 const Project = () => {
@@ -10,24 +11,9 @@ const Project = () => {
             <h1 className="project-content-title">Projects</h1>
             
             <div className="project-container">
-                <div className="project-card">
-                    <h2 className="project-title">Project Title</h2>
-                    <img className="project-image" src="https://i.ytimg.com/vi/WxmL6egpkKs/hqdefault.jpg" alt=""/>
-                    <p className="project-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-
-                    <div className="project-buttons">
-                        <a href="">
-                            <button>Demo</button>
-                        </a>
-                        <a href="">
-                            <button>Repo</button>
-                        </a>
-                        <a href="">
-                            <button>Site</button>
-                        </a>
-                    </div>
-                </div>
+                {
+                    information.projects.map(project => <ProjectCard project ={project}/>)
+                }
             </div>
         </div>
     )
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ProjectCard = (props) => {
+    const {title, projectImage, description, demoLink, repoLink, siteLink} = props.project
+    return (
+        <div className="project-card">
+            <h2 className="project-title">{title}</h2>
+            <img className="project-image" src={projectImage} alt={title}/>
+            <p className="project-description">{description}</p>
+
+            <div className="project-buttons">
+                <a target="_blank" href={demoLink}>
+                    <button>Demo</button>
+                </a>
+                <a target="_blank" href={repoLink}>
+                    <button>Repo</button>
+                </a>
+                <a target="_blank" href={siteLink}>
+                    <button>Site</button>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard

cbb5d53312d1c041137561690d9f6a7d2871248f
reireynoso <reireynoso@gmail.com>
2020-03-12 22:08:58 -0400

    removed basename parameter from hashrouter and deployed gh

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {HashRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
-    <Router basename="/">
+    <Router>
         <App /> 
     </Router>,
     document.getElementById('root')

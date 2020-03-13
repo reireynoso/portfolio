f3f6652536b6e299a61ae9dcb08e84040be5d961
reireynoso <reireynoso@gmail.com>
2020-03-12 21:44:59 -0400

    Updates

---
+++ precache-manifest.0a16d859653b96359daf6c00a61ad4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff4e2edb7b07672ae0143b35117578f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "886a9f537c1a41b02aab",
+    "url": "/portfolio/static/css/main.adc7b5dd.chunk.css"
+  },
+  {
+    "revision": "1410eaf8b0941abc6e3c",
+    "url": "/portfolio/static/js/2.fb9ffe62.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.fb9ffe62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886a9f537c1a41b02aab",
+    "url": "/portfolio/static/js/main.5e147501.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  }
+]);

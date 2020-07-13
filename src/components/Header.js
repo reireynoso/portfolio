24a1134f53ef3af59846c5799903739f564a18a6
reireynoso <reireynoso@gmail.com>
2020-07-12 23:48:13 -0400

    lienar gradient background and refactored css code. Changed Link to NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Header = ({pathname}) => {
     const path = pathname.replace("/", "")
     return (
         <div className="nav">
             <div className="page-links">
-                <Link className={`links ${path === "about"? "link-active" : ""}`} to="/about">About</Link>
-                <Link className={`links ${path === "projects"? "link-active" : ""}`} to="/projects">Projects</Link>
-                <Link className={`links ${path === "blogs"? "link-active" : ""}`} to="/blogs">Blogs</Link>
-                <Link className={`links ${path === "resume"? "link-active" : ""}`} to="/resume">Resume</Link>
+                <NavLink className="links" activeClassName="link-active" to="/about">About</NavLink>
+                <NavLink className="links" activeClassName="link-active" to="/projects">Projects</NavLink>
+                <NavLink className="links" activeClassName="link-active" to="/blogs">Blogs</NavLink>
+                <NavLink className="links" activeClassName="link-active" to="/resume">Resume</NavLink>
             </div>
         </div>
     )
